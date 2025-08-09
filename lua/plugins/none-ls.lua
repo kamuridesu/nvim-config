ecd7e47b28bc085ffce8da69e957181b845348aa
Kamuri Amorim <myk.gata14@gmail.com>
2025-08-09 20:50:48 -0300

    fix: updated via dotsyncer

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,18 @@
+return {
+	"nvimtools/none-ls.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.black,
+				null_ls.builtins.formatting.flake8,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.prettier,
+				null_ls.builtins.code_actions.refactoring,
+				null_ls.builtins.code_actions.gitsigns,
+			},
+		})
+	end,
+}
